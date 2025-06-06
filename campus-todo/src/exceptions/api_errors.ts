c4e48723309187a85226c6faa6a5c551e74fba6d
stepa <stepan.09myromec@gamil.com>
2025-06-06 22:04:41 +0500

    creating api_errors

---
+++ campus-todo/src/exceptions/api_errors.ts
@@ -0,0 +1,38 @@
+export class ApiError extends Error {
+    status: number;
+    errors: string[];
+
+    constructor(status: number, message: string, errors: string[] = []) {
+        super(message);
+        this.status = status;
+        this.errors = errors
+    }
+    static BadRequest(message: string, errors: string[] = []) {
+        return new ApiError(400, message, errors)
+    }
+
+    static Unauthorized(message: string = 'Пользователь не авторизован', errors: string[] = []) {
+        return new ApiError(401, message, errors)
+    }
+
+    static Forbidden(message: string = 'Доступ запрещен', errors: string[] = [] ) {
+        return new ApiError(403, message, errors)
+    }
+
+    static NotFound(message: string, errors: string[] = []) {
+        return new ApiError(404, message, errors);
+    }
+
+    static TooManyRequests(message = 'Слишком много запросов. Попробуйте позже', errors: string[] = []) {
+        return new ApiError(429, message, errors);
+  }
+
+    static BadConnectToDB(errors: string[] = []) {
+        return new ApiError(500, 'Ошибка при работе с базой данных', errors);
+    }
+
+    static Internal(message: string = "Ошибка сервера") {
+        return new ApiError(500, message);
+    }
+    
+}
